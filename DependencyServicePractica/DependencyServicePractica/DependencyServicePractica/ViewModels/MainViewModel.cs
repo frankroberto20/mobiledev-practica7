3192afb9593834180ca62c1aafbde413665860b3
frankchavez <robertofrank2000@hotmail.com>
2021-10-08 21:29:48 -0400

    Added ViewModel and connected to MainPage

--- DependencyServicePractica/DependencyServicePractica/DependencyServicePractica/ViewModels/MainViewModel.cs
+++ DependencyServicePractica/DependencyServicePractica/DependencyServicePractica/ViewModels/MainViewModel.cs
@@ -1,10 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using DependencyServicePractica.Services;
+using Xamarin.Forms;
+using Xamarin.Forms.Internals;
 
 namespace DependencyServicePractica.ViewModels
 {
     public class MainViewModel
     {
+        public MainViewModel()
+        {
+            Orientation = DependencyService.Get<IDeviceOrientationService>().GetOrientation();
+        }
+
+        public DeviceOrientation Orientation { get; set; }
     }
 }
